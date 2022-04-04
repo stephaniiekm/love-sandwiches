6775833bc239e59eb37b8a344c46f30877a67351
stephaniiekm <stephanie.karlsson.m@gmail.com>
2022-04-04 21:45:18 +0000

    Add a new API Key to see if it would help, but the issue was not that

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -12,16 +13,9 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-def get_sales_data():
-    """
-    Get sales figures input from the user
-    """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separeted by commas.")
-    print("Example: 10,20,30,40,50,60")
+sales = SHEET.worksheet('sales')
 
-    data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+data = sales.get_all_values()
 
 
-get_sales_data()
+print(data)
